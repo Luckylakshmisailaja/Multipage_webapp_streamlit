1b29dd9e1477fd5cc4c09f38a210ebff0d323d72
Luckylakshmisailaja <153287054+Luckylakshmisailaja@users.noreply.github.com>
2024-12-12 15:06:35 +0000

    Add files via upload

---
+++ multipagewebapp/views/about_me.py
@@ -0,0 +1,44 @@
+import streamlit as st 
+from forms.contact import contact_form
+
+@st.dialog("Contact Me")
+def show_contact_form():
+    contact_form()
+
+# --- HERO SECTION ---
+col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
+with col1:
+    st.image("./assets/Profile_Image.jpg", width=200)
+
+with col2:
+    st.title("Lakshmi Sailaja Kalisetti", anchor=False)
+    st.write(
+        "Junior Data Analyst, trying to prove my skills with passion."
+    )
+    if st.button("✉️ Contact Me"):
+        show_contact_form()
+        
+
+# --- EXPERIENCE & QUALIFICATIONS ---
+st.write("\n")
+st.subheader("Experience & Qualifications", anchor=False)
+st.write(
+    """
+    - 2.7 Years experience extracting actionable insights from data
+    - Strong hands-on experience and knowledge in Python and Excel
+    - Good understanding of statistical principles and their respective applications
+    - Excellent team-player and displaying a strong sense of initiative on tasks
+    """
+)
+
+# --- SKILLS ---
+st.write("\n")
+st.subheader("Hard Skills", anchor=False)
+st.write(
+    """
+    - Programming: Python (Scikit-learn, Pandas), SQL, VBA
+    - Data Visualization: PowerBi, MS Excel, Plotly
+    - Modeling: Logistic regression, linear regression, decision trees
+    - Databases: Postgres, MongoDB, MySQL
+    """
+)
